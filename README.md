3466e845c4a3a5f29fffe341fd8cf0432cf103ce
gaki2 <qudrkr0815@gmail.com>
2025-02-15 00:00:01 +0900

    feat: 리액트 컴포넌트도 처리할 수 있도록 만듦

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rehype-attr
 
+
+## 배포순서
+package.json 버전업
+1. npm login 
+2. npm publish --access=public
--- src/rehypeStagger.ts
+++ src/rehypeStagger.ts
@@ -5,37 +5,111 @@ import { visit } from 'unist-util-visit';
 type TagName = keyof HTMLElementTagNameMap;
 
 export type RehypeStaggerOptions = {
-  elements?: TagName[];
+  /**
+   * 시작 숫자
+   */
   startNum?: number;
+  /**
+   * 증가 숫자
+   */
   increment?: number;
+  /**
+   * 최소 숫자
+   */
   min?: number;
+  /**
+   * 최대 숫자
+   */
   max?: number;
-  shouldIncrease?: (currentElementTagName: string) => boolean;
+  /**
+   * stagger 증가 여부를 결정하는 함수
+   */
+  shouldIncrease?: (tagNameOrComponentName: string) => boolean;
+  /**
+   * stagger 를 적용할 element
+   * '*' 를 사용하면 모든 element에 적용
+   */
+  elements?: TagName[] | string;
+  /**
+   * stagger 를 적용할 mdx 컴포넌트 이름
+   * '*' 를 사용하면 모든 컴포넌트에 적용
+   */
+  componentNames?: string[] | string;
+  /**
+   * 리액트 컴포넌트에 stagger 값을 전달할 prop 이름
+   */
+  attributeName?: string;
 };
 
 export const rehypeStagger: Plugin<[RehypeStaggerOptions], Root> = (options = {}) => {
   const {
-    elements = ['p', 'h1', 'h2', 'h3', 'h4', 'h5'],
+    elements = '*',
     startNum = 1,
     increment = 1,
     min = 0,
     max = 99,
     shouldIncrease = () => true,
+    componentNames = '*',
+    attributeName = 'stagger',
   } = options;
 
   return (tree) => {
-    let count = startNum;
-    visit(tree, 'element', (node) => {
-      if (!elements.includes(node.tagName as TagName)) {
-        return;
-      }
+    let count = startNum - increment;
+    visit(tree, ['element', 'mdxJsxFlowElement'], (node) => {
+      
+      switch (node.type) {
+        case 'element': 
+          if (elements !== '*' && !elements.includes(node.tagName as TagName)) {
+            return;
+          }
 
-      if (shouldIncrease(node.tagName)) {
-        count += increment;
-        count = Math.max(Math.min(count, max), min);
-      }
+          if (shouldIncrease(node.tagName)) {
+            count += increment;
+            count = clamp(count, min, max);
+          }
+    
+          const style = node.properties?.style;
+          const styleValue = typeof style === 'object' 
+            ? Object.entries(style).map(([key, value]) => `${key}:${value}`).join(';')
+            : String(style || '');
+          
+          node.properties = { 
+            ...node.properties, 
+            style: withStagger(styleValue, count) 
+          };
+          break; 
 
-      node.properties = { ...node.properties, ...{ style: `--stagger:${count}` } };
+        // @ts-ignore
+        case 'mdxJsxFlowElement': 
+        // @ts-ignore
+          if (componentNames !== '*' && !componentNames.includes(node.name)) {
+            return;
+          }
+          // @ts-ignore
+          if (shouldIncrease(node.name)) {
+            count += increment;
+            count = clamp(count, min, max);
+          }
+          // @ts-ignore
+          node.attributes = node.attributes || [];
+          // @ts-ignore
+          node.attributes.push({
+            type: 'mdxJsxAttribute',
+            name: attributeName,
+            value: count,
+          });
+          
+          break;
+      }
     });
   };
 };
+
+
+const withStagger = (style: string, count: number) => {
+  return `--stagger:${count}; ${style}`.trim();
+}
+
+const clamp = (value: number, min: number, max: number) => {
+  return Math.max(Math.min(value, max), min);
+}
